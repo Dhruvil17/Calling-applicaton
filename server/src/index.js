519837e52590d0a146ac78f384a53f04e2711552
Dhruvil_Soni <dhruvilsoni1703@gmail.com>
2025-09-13 17:38:02 +0530

    add code

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = require("./router/router");
 const cors = require("cors");
-// Create Express webapp
+
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-// use the router we created in the for the endpoints 
+app.use(morgan("dev"));
 app.use(router);
 
-// Create http server and run it
+app.post("/", (req, res) => {
+  res.json({ message: "POST request received successfully!", data: req.body });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
+app.listen(port,() => {
     console.log(`Up and running on ${port}!`);
 })
